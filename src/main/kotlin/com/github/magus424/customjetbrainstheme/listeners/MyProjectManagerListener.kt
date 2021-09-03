aa6eb0c5f2790738a9b40f40044f4786f21706b1
GitHub Action <action@github.com>
2021-09-03 21:53:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/magus424/customjetbrainstheme/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/magus424/customjetbrainstheme/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.magus424.customjetbrainstheme.listeners
 
+import com.github.magus424.customjetbrainstheme.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/magus424/customjetbrainstheme/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.magus424.customjetbrainstheme.services
+
+import com.github.magus424.customjetbrainstheme.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
